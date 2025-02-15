d36654bd9ed68154edc4c831d11a93658cf47200
Rebecca Shillingford <51681289+rebeccashill@users.noreply.github.com>
2025-02-15 10:46:25 -0500

    Update filter2d.py
    Update Robot function to reflect improvements within the program.

--- Localization/2DParticleFilter/filter2d.py
+++ Localization/2DParticleFilter/filter2d.py
@@ -26,25 +26,48 @@ class Robot(Position):
         Position.__init__(self, pos)
         self.measurements = []
         self.max_measurement = 200
+        self.theta = np.array(poles)
+        self.speed_sigma = np.sum(poles)
+        self.theta_dot_sigma = np.dot(self.theta, self.speed_sigma)
+        self.angular_vel_variance = 0.1
+        self.linear_vel_variance = 0.3
 
     # Movement is perfectly accurate, even though we are assuming it isn't.
     def move(self, speed, theta_dot):
-        ### START STUDENT CODE
-        self.theta += 0
-        self.x += 0
-        self.y += 0
-        ### END STUDENT CODE
+        self.theta += theta_dot
+        self.x += math.cos(self.theta) * speed
+        self.y += math.sin(self.theta) * speed
+
+    def predict(self, speed, theta_dot):
+        theta_dot = r.normalvariate(theta_dot, self.theta_dot_sigma)
+        speed = r.normalvariate(speed, self.speed_sigma)
+        self.move(speed, theta_dot)
 
     def move_with_error(self, speed, theta_dot):
-        ### START STUDENT CODE
+        theta_dot = r.normalvariate(theta_dot, self.angular_vel_variance)
+        speed = r.normalvariate(speed, self.linear_vel_variance)
         self.move(speed, theta_dot)
-        ### END STUDENT CODE
 
     # Measurement is perfectly accurate even though we are assuming it isn't.
     def measure(self, poles):
-        ### START STUDENT CODE
-        self.measurements = []
-        ### END STUDENT CODE
+        self.measurements.clear()
+
+        for pole in poles:
+            diff_x = pole.x - self.x
+            diff_y = pole.y - self.y
+
+            distance = math.sqrt(diff_x**2 + diff_y**2)
+
+            if distance < self.max_measurement:
+                angle = math.atan2(diff_y, diff_x)
+                angle -= self.theta
+
+                if angle > 2*math.pi:
+                    angle -= 2*math.pi
+                elif angle < 2*math.pi:
+                    angle += 2*math.pi
+
+                self.measurements.append(Measurement(distance, angle))
 
 
 class Particle(Robot):
